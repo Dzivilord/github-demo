f66832910a89525b3ed32e2564afc0e13434ddbe
Dzivilord <126346784+Dzivilord@users.noreply.github.com>
2024-09-18 11:37:07 +0700

    Add files via upload

---
+++ custom.py
@@ -0,0 +1,18 @@
+
+
+class Custom:
+    def __init__(self,idAccount,name,address,typeSaving,identify,openDate):
+        self.idAccount=idAccount
+        self.name=name
+        self.address=address
+        self.typeSaving=typeSaving
+        self.identify=identify
+        self.openDate=openDate
+    
+
+class Solution:
+    pass
+
+Loi=Custom(23,"Gaylord",'DN City','12 months','22120188','17/9/2024')
+print(Loi.address)
+
